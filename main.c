ec9acfaceb263e98a5b870183ad77ad445f2251b
MALCOLMOTIENO <162960877+MALCOLMOTIENO@users.noreply.github.com>
2024-03-11 00:13:08 -0700

    initial commit
    Scanfmodified

--- main.c
+++ main.c
@@ -3,12 +3,14 @@
 
 int main()
 {
-    int a, b, c;
-    printf("Enter the first value:");
+    int a, b, c, d;
+    printf("Enter the first value: \n");
     scanf("%d", &a);
-    printf("Enter the second value:");
+     printf("Enter the second value: \n");
     scanf("%d", &b);
-    c = a + b;
-    printf("%d + %d = %d\n", a, b, c);
+     printf("Enter the third value: \n");
+    scanf("%d", &c);
+    d = a + b + c;
+    printf("%d + %d + %d = %d\n", a, b, c, d);
     return 0;
 }
